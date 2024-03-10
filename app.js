284b3926131a52ff30240f37acb841ff404442b9
Rivki <rivkiii770@gmail.com>
2024-03-10 17:27:09 +0200

    Added more solutions

--- app.js
+++ app.js
@@ -18,5 +18,49 @@ function plusMinus(arr) {
   console.log((count[1] / countAllNums).toFixed(6));
   console.log((count[2] / countAllNums).toFixed(6));
 }
+//plusMinus([-4, 3, -9, 0, 4, 1]);
 
-plusMinus([-4, 3, -9, 0, 4, 1]);
+// Link to question: https://www.hackerrank.com/challenges/one-week-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+function miniMaxSum(arr) {
+  let sum = arr[0],
+    min = arr[0],
+    max = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    sum += arr[i];
+    if (arr[i] < min) {
+      min = arr[i];
+    } else if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  let minimumSum = sum - max;
+  let maximumSum = sum - min;
+  console.log(minimumSum, maximumSum);
+}
+
+//miniMaxSum([1, 3, 5, 7, 9]);
+
+function fizzBuzz(n) {
+  for (let i = 1; i < n + 1; i++) {
+    if (n % 3 === 0 && n % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if (n % 3 === 0) {
+      console.log('Fizz');
+    } else if (n % 5 === 0) {
+      console.log('Buzz');
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// fizzBuzz(15);
+
+function findMedian(arr) {
+  const sortedArr = arr.toSorted((a, b) => a - b);
+  const medianPlace = Math.floor(arr.length / 2);
+
+  return sortedArr[medianPlace];
+}
+
+console.log(findMedian([5, 6, 9, 3, 2]));
